e0053c24a488598094d70aa2715e4b7d3ebf9c1b
MthokozisiZwane <mhokozisi.zwane1999@gmail.com>
2024-04-01 19:24:13 +0000

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,9 +3,9 @@
 Simple helper function for pagination
 """
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """Returns a tuple of start and end indexes for pagination"""
     start_index = (page - 1) * page_size
     end_index = page * page_size
     return start_index, end_index
-
